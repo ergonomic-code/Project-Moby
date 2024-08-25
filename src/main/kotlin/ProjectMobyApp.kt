81b833af93efcace6f5cf066bc7ce7564e731424
Aleksey Zhidkov <me@azhidkov.pro>
2024-08-25 10:36:15 +0700

    Добавлен скелет приложения

---
+++ src/main/kotlin/ProjectMobyApp.kt
@@ -0,0 +1,11 @@
+package pro.azhidkov.training.project_moby
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectMobyApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjectMobyApplication>(*args)
+}
